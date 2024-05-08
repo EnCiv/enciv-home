8148feb65617c4907b15746ad5394e858480ba1e
David Fridley <ddfridley@yahoo.com>
2024-05-08 16:48:24 -0700

    no browserConfig warning in markdown

--- app/web-components/markdown.js
+++ app/web-components/markdown.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import MarkdownBlock from '../components/markdown-block'
 
 export default function Markdown(props) {
-  const { subject, description, location, ...otherProps } = props
+  const { subject, description, location, browserConfig, ...otherProps } = props
   return <MarkdownBlock children={description} {...otherProps} />
 }
