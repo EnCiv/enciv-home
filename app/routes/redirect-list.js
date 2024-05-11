2e71d287d1464fb751eb7109ecaac0bdd8b64b4e
David Fridley <ddfridley@yahoo.com>
2024-05-10 21:10:13 -0700

    redirects route

---
+++ app/routes/redirect-list.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// maybe in the future this will come out of the environment, or a database, or a file or all
+
+const list = [{ path: '/undebates', dst: 'https://cc.enciv.org/undebates' }]
+
+export default function redirectList() {
+  for (const red of list) {
+    this.app.get(red.path, (req, res, next) => {
+      try {
+        res.redirect(red.dst)
+      } catch (error) {
+        this.emit('error', error)
+      }
+    })
+  }
+}
