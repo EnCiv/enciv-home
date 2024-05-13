d0c91e27e5a34f391515a59e39f77b0e0eb3de17
David Fridley <ddfridley@yahoo.com>
2024-05-13 12:06:14 -0700

    wp article formatting

--- app/components/wp-fetch.js
+++ app/components/wp-fetch.js
@@ -1,30 +1,37 @@
-import React, {useEffect, useState} from 'react' 
+import React, { useEffect, useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import ReactHtmlParser from 'react-html-parser'
-const apiFetch=require('@wordpress/api-fetch').default
+const apiFetch = require('@wordpress/api-fetch').default
 
-export default function WpFetch(props){
-    const {position=0}=props
-    const [article,setArticle]=useState(null)
-    const classes=useStylesFromThemeFunction()
-    useEffect(()=>{
-        apiFetch( { path: 'https://enciv.org/wp-json/wp/v2/posts' }, {mode: 'no-cors'} ).then( ( posts ) => {
-            if(posts.length && posts[position]){
-                const rendered=posts[position].content.rendered
-                const html=rendered.replace(/\[.*?\]/g,'')
-                setArticle(<div className={classes.wpFetch}>{ReactHtmlParser(html)}</div>)
-            }
-        } );
+export default function WpFetch(props) {
+  const { position = 0 } = props
+  const [article, setArticle] = useState(null)
+  const classes = useStylesFromThemeFunction()
+  useEffect(() => {
+    apiFetch({ path: 'https://enciv.org/wp-json/wp/v2/posts' }, { mode: 'no-cors' }).then(posts => {
+      if (posts.length && posts[position]) {
+        setArticle(posts[position])
+      }
     })
-    return article
+  }, [position])
+  if (!article) return null
+
+  const rendered = article.content.rendered
+  const html = rendered.replace(/\[.*?\]/g, '')
+  return (
+    <div className={classes.wpFetch}>
+      <h1>{article.title.rendered}</h1>
+      <div>{ReactHtmlParser(html)}</div>
+    </div>
+  )
 }
 
 const useStylesFromThemeFunction = createUseStyles(theme => ({
-    wpFetch: {
-        '& img': {
-            float: 'left',
-            paddingRight: '1rem',
-            paddingBottom: '1rem'
-        }
-    }
+  wpFetch: {
+    '& img': {
+      float: 'left',
+      paddingRight: '1rem',
+      paddingBottom: '1rem',
+    },
+  },
 }))
