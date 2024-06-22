5b16d878fd2c91fbe018a225fda4cef789701430
Bara Ahmad <bara.alsaied@gmail.com>
2024-06-22 16:49:12 -0400

    unicode to ascii

--- app/web-components/article.js
+++ app/web-components/article.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ArticleBlock from '../components/article-block'
 import { BrevoHelmet } from '../components/brevo-join'
+import { Helmet } from 'react-helmet'
+// import { ConvertUnicode } from '../components/convert-unicode'
 
 export default function Article(props) {
   const { subject, description, location, ...otherProps } = props
+  function convertUnicode(input) {
+    return input.replace(/&#(\d+);/g, (m, d) => String.fromCharCode(d))
+  }
+
   return (
     <>
       <BrevoHelmet />
+      <Helmet>
+        <title>{convertUnicode(subject)}</title>
+      </Helmet>
       <ArticleBlock mode="light" {...otherProps} />
     </>
   )
