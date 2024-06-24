53d421886785ae23369f6e76ffbc51dd073c4777
Bara Ahmad <bara.alsaied@gmail.com>
2024-06-24 11:53:05 -0400

    added github issue link and removed unnecessary comment

--- app/web-components/article.js
+++ app/web-components/article.js
@@ -1,8 +1,8 @@
+//https://github.com/EnCiv/enciv-home/issues/25
 import React from 'react'
 import ArticleBlock from '../components/article-block'
 import { BrevoHelmet } from '../components/brevo-join'
 import { Helmet } from 'react-helmet'
-// import { ConvertUnicode } from '../components/convert-unicode'
 
 export default function Article(props) {
   const { subject, description, location, ...otherProps } = props
