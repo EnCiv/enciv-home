6d9ffb9167c1ad978f34fe3fd1370c89ea7734cb
Yufeng Wang <wang.yufeng@northewastern.edu>
2024-07-29 13:15:21 -0700

    revert changes

--- app/routes/redirect-list.js
+++ app/routes/redirect-list.js
@@ -2,16 +2,13 @@
 
 // maybe in the future this will come out of the environment, or a database, or a file or all
 
-import getSitemap from './sitemap'
-const list = [{ path: '/undebates', dst: 'https://cc.enciv.org/undebates' }]
+const list = [{ path: '/undebates', dst: 'https://cc.enciv.org/sitemap.xml' }]
 
 export default function redirectList() {
   for (const red of list) {
     this.app.get(red.path, (req, res, next) => {
       try {
-        getSitemap().then(() => {
-          res.redirect(red.dst)
-        })
+        res.redirect(red.dst)
       } catch (error) {
         this.emit('error', error)
       }
