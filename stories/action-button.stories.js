8b7ff905094d299d60dc1b9aaddcc4e1801ae17b
Bara Ahmad <bara.alsaied@gmail.com>
2024-09-08 08:25:22 -0400

    Action button with icon

--- stories/action-button.stories.js
+++ stories/action-button.stories.js
@@ -3,6 +3,7 @@ import { expect } from '@storybook/jest'
 import ActionButton from '../app/components/action-button'
 import { BrevoHelmet } from '../app/components/brevo-join'
 import { userEvent, within } from '@storybook/testing-library'
+import SvgDonate from '../app/svgr/donate'
 
 //import { onDoneDecorator, onDoneResult} from 'civil-pursuit/stories/common'
 
@@ -46,7 +47,16 @@ export const TransparentMode = {
   args: { children: 'click to EnCiv.org', action: 'https://enciv.org', mode: 'transparent' },
   parameters: {
     backgrounds: {
-      default: 'dark'
-    }
-  }
+      default: 'dark',
+    },
+  },
+}
+
+export const ActionButtonWithIcon = {
+  args: { actionText: 'Donate', mode: 'transparent', iconName: 'Donate' },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
 }
